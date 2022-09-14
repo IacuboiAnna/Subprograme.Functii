194b005d2c001dc40bc9dfc7d594f6f44a3aa5da
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2022-09-14 09:40:26 +0300

    Update lista_float.py

--- lista_float.py
+++ lista_float.py
@@ -4,7 +4,10 @@ def lista_float():
     l_float=[]
     for i in range(n):
         elem=float(input('Elementul '+str(i)+' este:'))
-        l_float.append(elem)
+        if isinstance(elem,float)==True:
+            l_float.append(elem)
+        else:
+            print("Restartati si introduceti un numar real")
     return l_float
 
 print(lista_float())
